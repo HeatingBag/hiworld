e4e66b7992c1057506b34f7129ed9e03af83f5f6
HeatingBag <2606224360@qq.com>
2022-06-21 16:49:18 +0800

    test

---
+++ src/Hiworld.java
@@ -0,0 +1,12 @@
+/**
+ * @Title: Hiworld
+ * @Auther: zhang
+ * @Version: 1.0
+ * @create: 2022/6/21 16:44
+ */
+public class Hiworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hi World");
+    }
+}
